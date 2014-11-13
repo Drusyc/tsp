ed12ed4138713bec2bf940a2db19a1d1413b36d0
utilisateur <brazousd@telesun.imag.fr>
2014-11-13 17:17:36 +0100

    qsort

--- approx.c
+++ approx.c
@@ -21,5 +21,36 @@ double couts[4][4] = 	{{0, 1, 2, 5},
 	
 */
 
+void sort(arc * ar, int l, int h){
+  int i, j
+  arc wsp, m;
+  
+ i=l; 
+ j=h;
+ m=ar[(int)(double_rand(1)*(h-l)+l)]; 
+
+     do
+      while (ar[i].cout < m.cout) i++;    
+      while (ar[j].cout > m.cout) j--;    
+        if (i<=j){
+          wsp=ar[i];
+	  ar[i]=ar[j];
+	  ar[j]=wsp;
+          i++;
+	  j--;
+	}
+     while (i <= j);
+     
+      if (i<h)
+	sort(ar, i, h);
+      if (l<j)
+	sort(ar, l, j);
+  
+}
+ 
+void qsort(arc* ar, int n){
+ srand (time(NULL));
+ sort(ar, 0, n-1);
+}
 
 
